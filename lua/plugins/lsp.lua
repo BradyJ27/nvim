c09d904a7267ba41fe3ce3320cca8d6c2149ecd5
Brady Johnson <bradyjohnson84@gmail.com>
2024-04-21 20:27:35 -0600

    Add automatic lsp setup

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,9 +7,14 @@ return {
 
 	config = function()
 		require("mason").setup()
-		require("mason-lspconfig").setup {
-			ensure_installed = { "lua_ls", "pyright" },
-		}
-		require("lspconfig").lua_ls.setup {}
-	end
+		require("mason-lspconfig").setup({
+			ensure_installed = { "bashls", "dockerls", "lua_ls", "pyright", "yamlls" },
+		})
+		require("mason-lspconfig").setup_handlers({
+			function(server_name)
+				require("lspconfig")[server_name].setup({})
+			end,
+		})
+		require("lspconfig").lua_ls.setup({})
+	end,
 }
