31adeb4d0202633eed3bd5bb7369313372f6c202
BradyJ27 <bradyjohnson84@gmail.com>
2024-05-14 17:58:45 -0600

    Add transparent colorscheme; Add copilot

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,7 +4,8 @@ return {
 		priority = 1000,
 		config = function()
 			require("onedark").setup({
-				style = "dark",
+				style = "deep",
+				transparent = "true",
 			})
 			require("onedark").load()
 		end,
